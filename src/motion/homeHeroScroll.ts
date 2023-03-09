8ab5a7d6fa479f5348af4f28f0c2cd9613a9e7e5
samsostudios <sam@samsostudios.com>
2023-03-09 13:34:36 -0700

    gsap business test

--- src/motion/homeHeroScroll.ts
+++ src/motion/homeHeroScroll.ts
@@ -8,7 +8,7 @@ export const heroScroll = () => {
       trigger: homeHero,
       start: 'top top',
       end: '50% top',
-      //   markers: true,
+      // markers: true,
       toggleActions: 'play none none reverse',
       scrub: 1,
     },
@@ -17,9 +17,20 @@ export const heroScroll = () => {
   const heroBg = document.querySelector('.home-hero_bg-mask');
   animation.to(heroBg, {
     width: '50%',
-    height: '85%',
-    borderRadius: '4rem',
+    height: '80%',
+    borderRadius: '100vw 100vw 2rem 2rem',
     duration: 1,
+  });
+
+  const scrollIndicator = gsap.timeline({
+    scrollTrigger: {
+      trigger: homeHero,
+      start: 'top top',
+      end: '50% top',
+      markers: true,
+      toggleActions: 'play none none reverse',
+      scrub: 1,
+    },
   });
 };
 
@@ -31,7 +42,7 @@ export const heroHide = () => {
       end: '50% top',
       toggleActions: 'play none none reverse',
       scrub: true,
-      //   markers: true,
+      // markers: true,
     },
   });
 
--- src/motion/navMotion.ts
+++ src/motion/navMotion.ts
@@ -1,4 +1,7 @@
 import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
+gsap.registerPlugin(SplitText);
 
 const body = document.body as HTMLElement;
 const navMenu = document.querySelector('#navMenu');
@@ -126,6 +129,10 @@ export const menuMotionClose = () => {
   animation.to(navSpan, { duration: 0.4, width: '0%', ease: 'expo.inOut' }, '<');
 
   animation.to(navMenu, { duration: 0.6, width: '0%', ease: 'power3.inOut' }, '-=0.6');
+
+  const navButtonAnimation = gsap.timeline();
+  const splitOpen = new SplitText('#menuText', { type: 'words,chars' });
+  console.log('split open', splitOpen);
 
   return animation;
 };
