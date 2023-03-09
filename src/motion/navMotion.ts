20afcc0e63e7be4d5c9d79026aa434c30dab2e44
samsostudios <sam@samsostudios.com>
2023-03-09 15:32:39 -0700

    test

--- src/motion/navMotion.ts
+++ src/motion/navMotion.ts
@@ -130,7 +130,7 @@ export const menuMotionClose = () => {
 
   animation.to(navMenu, { duration: 0.6, width: '0%', ease: 'power3.inOut' }, '-=0.6');
 
-  // const navButtonAnimation = gsap.timeline();
+  const navButtonAnimation = gsap.timeline();
   const splitOpen = new SplitText('#menuText', { type: 'words,chars' });
   console.log('split open', splitOpen);
 
